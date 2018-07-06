fa0553deeb420c88e078737af016dbdfc4f0dd97
smrkem <smrkem@gmail.com>
2018-07-05 22:23:41 -0400

    first repo commit

--- static.config.js
+++ static.config.js
@@ -1,12 +1,13 @@
 import { reloadRoutes } from 'react-static/node'
 import jdown from 'jdown'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 import chokidar from 'chokidar'
 
 chokidar.watch('content').on('all', () => reloadRoutes())
 
 export default {
   getSiteData: () => ({
-    title: 'React Static',
+    title: 'Just In Case | Doomsday Prepping Made Easy',
   }),
   getRoutes: async () => {
     const { posts, home, about } = await jdown('content')
@@ -44,5 +45,56 @@ export default {
         component: 'src/containers/404',
       },
     ]
+  },
+  webpack: (config, { defaultLoaders, stage }) => {
+    let loaders = []
+
+    if (stage === 'dev') {
+      loaders = [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }]
+    } else {
+      loaders = [
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+            minimize: stage === 'prod',
+            sourceMap: false,
+          },
+        },
+        {
+          loader: 'sass-loader',
+          options: { includePaths: ['src/'] },
+        },
+      ]
+
+      // Don't extract css to file during node build process
+      if (stage !== 'node') {
+        loaders = ExtractTextPlugin.extract({
+          fallback: {
+            loader: 'style-loader',
+            options: {
+              sourceMap: false,
+              hmr: false,
+            },
+          },
+          use: loaders,
+        })
+      }
+    }
+
+    config.module.rules = [
+      {
+        oneOf: [
+          {
+            test: /\.s(a|c)ss$/,
+            use: loaders,
+          },
+          defaultLoaders.cssLoader,
+          defaultLoaders.jsLoader,
+          defaultLoaders.fileLoader,
+        ],
+      },
+    ]
+    return config
   },
 }
